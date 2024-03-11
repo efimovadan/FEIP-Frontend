ac158141ab815ed6e2f7a0614c23428533a03e16
efimovadan <duqwin22@gmail.com>
2024-03-11 17:22:13 +1000

    added header and content components

--- app.vue
+++ app.vue
@@ -1,24 +1,22 @@
 <template>
-  <h1 class="montserrat color-blue">Hello, world!</h1>
-  <h3 class="open-sans color-red">Strawberry</h3>
+  <Header/>
+  <Content/>
 </template>
 
 <script setup>
-  console.log("Hello, world!");
+import Header from '~/components/Header.vue'
+import Content from '~/components/Content.vue'
 </script>
 
-<style>
-.color-red {
-  color: rgb(226, 41, 41);
-}
-.color-blue {
-  color: rgb(70, 84, 233);
-}
-.montserrat {
-  font-family: Montserrat;
+<style lang="scss">
+:root {
+  --font-title: "Open sans", sans-serif;
+  --font-text: Montserrat, sans-serif;
+
+   font-family: var(--font-text);
 }
 
-.open-sans {
-  font-family: "Open sans";
+body {
+  margin: 0px;
 }
 </style>
---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="header">
+        Header
+    </h1>
+</template>
+
+<style>
+
+.header {
+    padding: 20px;
+    padding-left: 10%;
+    font-weight: bold;
+    margin: 0;
+    font-family: var(--font-title);
+    background-color: rgb(152, 208, 249);
+}
+</style>
