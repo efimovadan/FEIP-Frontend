5a68b2bbeb24b263633bb7210fce6124750b4ce2
efimovadan <duqwin22@gmail.com>
2024-03-11 14:52:21 +1000

    fixed 1 hw: removed comma in classes and added fonts to dependencies

--- app.vue
+++ app.vue
@@ -1,16 +1,13 @@
 <template>
-  <h1 class="montserrat, color-blue">Hello, world!</h1>
-  <h3 class="open-sans, color-red">Strawberry</h3>
+  <h1 class="montserrat color-blue">Hello, world!</h1>
+  <h3 class="open-sans color-red">Strawberry</h3>
 </template>
 
-<script>
-console.log("Hello, world!");
+<script setup>
+  console.log("Hello, world!");
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
-
 .color-red {
   color: rgb(226, 41, 41);
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Open Sans': [400, 700],
+        Montserrat: true,
+      }
+    }]
+  ]
+});
